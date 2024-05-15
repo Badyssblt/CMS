2ecf4a7ad1acd219e9ab0f60bd59b2d64b6d1259
Mirooyo <badtruc@gmail.com>
2024-05-15 11:59:57 +0200

    project initialized

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <sidebar/>
+</template>
+
+<script>
+import Sidebar from '~/components/sidebar.vue'
+
+export default {
+    name: "index",
+    components:  {
+        Sidebar
+    }
+}
+</script>
+
+<style>
+
+</style>
