4ac0b5893dc432a9d6887806860b0a57ad3470a1
Badyss <badyss.blt@gmail.com>
2024-05-21 07:38:20 +0200

    Edit content in component

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 <div class="flex flex-row">
     <sidebar/>
     <base-structure-vue :data="data" class="flex-1"/>
+    <validate-changement-component :componentName="'basicHeader'" :type="'header'"/>
 </div>
 
 </template>
@@ -10,12 +11,15 @@
 import nuxtStorage from "nuxt-storage";
 import BaseStructureVue from '~/components/structures/BaseStructure.vue'
 import Sidebar from '~/components/sidebar.vue'
+import ValidateChangementComponent from '../components/forms/ValidateChangementComponent.vue';
 
 export default {
     name: "index",
     components:  {
         BaseStructureVue,
-        Sidebar    },
+        Sidebar,
+        ValidateChangementComponent    
+    },
     setup(){
 
         const data = ref({});
--- server/api/writeFile.js
+++ server/api/writeFile.js
@@ -1,33 +1,21 @@
 import fs from "fs";
 import path from "path";
-import cheerio from "cheerio";
 
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event); // Lire les données POST
-  const { selector, newContent } = body; // `selector` pour cibler l'élément, `newContent` pour le nouveau contenu HTML
+  const body = await readBody(event);
+  const { componentName, currentComponent } = body;
 
   const filePath = path.join(
     process.cwd(),
-    "components/headers/basicHeader.vue"
+    `components/headers/${componentName}.vue`
   );
 
   try {
-    // Lire le contenu du fichier
     const data = await fs.promises.readFile(filePath, "utf-8");
 
-    // Charger le contenu dans cheerio
-    const $ = cheerio.load(data, { xmlMode: true });
+    const tempData = `<template>${currentComponent}</template>`;
 
-    // Remplacer le contenu de l'élément cible
-    $(
-      `<li class="editable focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white bg-indigo-600 cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded"></li>`
-    ).text("test2");
-
-    // Obtenir le contenu modifié
-    const modifiedData = $.html();
-
-    // Écrire le contenu modifié de retour dans le fichier
-    await fs.promises.writeFile(filePath, modifiedData, "utf-8");
+    await fs.promises.writeFile(filePath, tempData, "utf-8");
 
     return { success: true, message: "Fichier modifié avec succès." };
   } catch (err) {
